545c93026d51119c00f8994eaa56483c12cc3878
Jacob Gil <jacobgil325@gmail.com>
2021-09-17 15:53:26 -0400

    added sections

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -14,6 +14,47 @@ import {
   ListTitle,
 } from "./TechnologiesStyles";
 
-const Technologies = () => <div>Technologies</div>;
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      In my short time in the software development field, I have already worked
+      with various technologies all the way from Back-end to Design and
+      Front-end
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br /> React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with Java, <br /> Python, and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Mobile-App Dev</ListTitle>
+          <ListParagraph>
+            Experience with SwiftUI,
+            <br /> Kotlin, Dart/Flutter
+            <br /> (Learning React Native)
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
 
 export default Technologies;
