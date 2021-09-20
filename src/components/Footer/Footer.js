8a6745f9ede6a7a75f8f1e010515afe736403c7d
Jacob Gil <59539299+jgil325@users.noreply.github.com>
2021-09-19 23:03:52 -0400

    footer done. (#7)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:678-206-4017">678-206-4017</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:Jacob.Gil@uga.edu">Jacob.Gil@uga.edu</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Learning more day by day</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons
+            href="https://github.com/jgil325"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons
+            href="https://www.linkedin.com/in/gil-jacob/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons
+            href="https://www.instagram.com/jgil325/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
