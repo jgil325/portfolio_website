dadccc123a3d3169609afc53ec089329ff8feb45
Jacob Gil <59539299+jgil325@users.noreply.github.com>
2021-09-19 23:18:50 -0400

    Fixing accomplishments (#8)
    * fixed box heights
    
    * fixed boxes chart to have 2 per row

--- src/components/Accomplishments/AccomplishmentsStyles.js
+++ src/components/Accomplishments/AccomplishmentsStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Boxes = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   gap: 24px;
   margin: 24px 0 40px;
 
@@ -25,7 +25,7 @@ export const Boxes = styled.div`
 export const Box = styled.div`
   background: #212d45;
   border-radius: 12px;
-  height: 144px;
+  height: 244px;
   padding: 24px;
   @media ${(props) => props.theme.breakpoints.lg} {
     height: 210px;
